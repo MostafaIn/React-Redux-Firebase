e8d8741c23554cbbfc7333c8846a2344d3a7d8c2
MostafaIn <mostafa.hazareh@integrify.fi>
2019-07-12 19:02:39 +0300

    the necessory components are ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="nav-wrapper blue darken-2">this is React Redux and Firebase practice.</h1>
+    </div>
+  );
+}
+
+export default App;
