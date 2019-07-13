9bcb6604897e8deb5262baf5bd8e874734731576
MostafaIn <mostafa.hazareh@integrify.fi>
2019-07-13 15:03:25 +0300

    apply redux

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
       <div className="card z-depth-0 project-summary">
           <div className="card-content gray-text text-darken-3">
-              <div className="card-title">Proejct Title</div>
-              <p>Posted by thenetninja website</p>
+              <div className="card-title">{project.title}</div>
+              <p>{project.content}</p>
               <p className="gray-text">3rd September , 2am</p>
           </div>
       </div>
---
+++ src/components/store/reducers/projectReducer.jsx
@@ -0,0 +1,18 @@
+
+const initState={
+    projects:[
+        {id:'1',title:'my portfolio',content:'this can reveal my knowledge and skills in web developing.'},
+        { id: '2', title: 'learning material UI', content: 'I would like to do the design with this tool.' },
+        { id: '3', title: 'Student assessment system', content: 'this is a testing and exam system for the students and teachers.' },
+    ]
+}
+
+const projectReducer = (state=initState,action)=>{
+    switch(action.type){
+        case 'CREATE_PROJECT':
+        console.log('created project :)', action.project)
+    }
+    return state;
+}
+
+export default projectReducer
