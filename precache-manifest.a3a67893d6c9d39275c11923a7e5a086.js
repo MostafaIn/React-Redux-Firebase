dbb0cae9d2ef8f8aedad6f4bb355c551e82e17c9
MostafaIn <mostafa.hazareh@integrify.fi>
2019-07-16 12:17:42 +0300

    Updates

---
+++ precache-manifest.a3a67893d6c9d39275c11923a7e5a086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029d062a1f2e3c6ab5e240fef9886e4f",
+    "url": "/React-Redux-Firebase/index.html"
+  },
+  {
+    "revision": "a60363f0efa7784a7f34",
+    "url": "/React-Redux-Firebase/static/css/main.9f681e75.chunk.css"
+  },
+  {
+    "revision": "03ba6e3cbe8e6e31d0fd",
+    "url": "/React-Redux-Firebase/static/js/2.8f2fe8ba.chunk.js"
+  },
+  {
+    "revision": "a60363f0efa7784a7f34",
+    "url": "/React-Redux-Firebase/static/js/main.b911fb5b.chunk.js"
+  },
+  {
+    "revision": "c0d142cab24c815dbec8",
+    "url": "/React-Redux-Firebase/static/js/runtime~main.a144d9a4.js"
+  }
+]);
